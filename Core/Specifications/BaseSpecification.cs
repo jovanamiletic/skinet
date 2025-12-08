b4a27c0b45b1792cbd4ce9aeec152106c27dba04
Jovana Miletic <jovanamiletic20@gmail.com>
2025-12-08 18:33:44 +0100

    End of section 5

--- Core/Specifications/BaseSpecification.cs
+++ Core/Specifications/BaseSpecification.cs
@@ -15,6 +15,21 @@ public class BaseSpecification<T>(Expression<Func<T, bool>>? criteria) : ISpecif
 
   public bool IsDistinct { get; private set; }
 
+  public int Take { get; private set; }
+
+  public int Skip { get; private set; }
+
+  public bool isPagingEnabled { get; private set; }
+
+  public IQueryable<T> ApplyCriteria(IQueryable<T> query)
+  {
+    if (Criteria != null)
+    {
+      query = query.Where(Criteria);
+    }
+    return query;
+  }
+
   protected void AddOrderBy(Expression<Func<T, object>> orderByExpression)
   {
     OrderBy = orderByExpression;
@@ -28,8 +43,14 @@ public class BaseSpecification<T>(Expression<Func<T, bool>>? criteria) : ISpecif
   {
     IsDistinct = true;
   }
-}
 
+  protected void ApplyPaging(int skip, int take)
+  {
+    Skip = skip;
+    Take = take;
+    isPagingEnabled = true;
+  }
+}
 
 public class BaseSpecification<T, TResult>(Expression<Func<T, bool>> criteria)
             : BaseSpecification<T>(criteria), ISpecification<T, TResult>
--- Infrastructure/Data/SpecificationEvaluator.cs
+++ Infrastructure/Data/SpecificationEvaluator.cs
@@ -29,6 +29,11 @@ public class SpecificationEvaluator<T> where T : BaseEntity
       query = query.Distinct();
     }
 
+    if (spec.isPagingEnabled)
+    {
+      query = query.Skip(spec.Skip).Take(spec.Take);
+    }
+
     return query;
   }
 
@@ -59,6 +64,11 @@ public class SpecificationEvaluator<T> where T : BaseEntity
     if (spec.IsDistinct)
     {
       selectQuery = selectQuery?.Distinct();
+    }
+
+    if (spec.isPagingEnabled)
+    {
+      selectQuery = selectQuery?.Skip(spec.Skip).Take(spec.Take);
     }
 
     return selectQuery ?? query.Cast<TResult>();
